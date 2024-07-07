901ad96fecd4849703c7f8d472393ff7de534a65
Gaurav <royalgk2@gmail.com>
2024-07-07 22:55:32 +0530

    build static layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import Square from './square/Square';
 import './App.css';
+import { useState } from 'react';
+
+const renderFrom=[[1,2,3],[4,5,6],[7,8,9]];
 
 function App() {
+  
+  const [gameState,setGameState]=useState(renderFrom);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-div">
+    <div className='move-detection'>
+          <div className='left'>You</div>
+          <div className='right'>Opponent</div>
+     </div>
+     <div>
+     
+     <h1 className='water-background game-heading'>TIC TAC TOE</h1>
+     <div className='square-wrapper'>
+         {
+           renderFrom.map( (arr) =>
+             arr.map((e)=>{
+              return <Square  setGameState={setGameState} id={e} key={e}/>
+             })
+           )
+         }
+     </div>
+     </div>
     </div>
   );
 }
